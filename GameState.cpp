0513fb8b9ab5d55a8b087a7fdc2a5356f4e08b95
Rahul Banerjee <rbanerjee28@gmail.com>
2022-05-13 22:48:14 +0200

    added game over state.

---
+++ GameState.cpp
@@ -0,0 +1,42 @@
+#include <sstream>
+#include "GameState.hpp"
+#include "DEFINITIONS.hpp"
+#include <iostream>
+
+namespace Game
+{
+    GameState::GameState(GameDataRef data) : _data(data)
+    {
+    }
+
+    void GameState::Init()
+    {
+        _data->assets.LoadTexture("Game Background", GAME_BACKGROUND_FILEPATH);
+
+        _background.setTexture(this->_data->assets.GetTexture("Game Background"));
+    }
+
+    void GameState::HandleInput()
+    {
+        sf::Event event;
+
+        while (_data->window.pollEvent(event))
+        {
+            if (sf::Event::Closed == event.type)
+            {
+                _data->window.close();
+            }
+        }
+    }
+
+    void GameState::Update(float dt)
+    {
+    }
+
+    void GameState::Draw(float dt)
+    {
+        _data->window.clear();
+        _data->window.draw(_background);
+        _data->window.display();
+    }
+}
--- MainMenuState.cpp
+++ MainMenuState.cpp
@@ -1,5 +1,6 @@
 #include <sstream>
 #include "MainMenuState.hpp"
+#include "GameState.hpp"
 #include "DEFINITIONS.hpp"
 #include <iostream>
 
@@ -37,9 +38,9 @@ namespace Game
                 _data->window.close();
             }
 
-            if(_data->input.IsSpriteClicked(_playButton, sf::Mouse::Left, _data->window))
+            if (_data->input.IsSpriteClicked(_playButton, sf::Mouse::Left, _data->window))
             {
-                std::cout<<"Go To Game Screen"<<std::endl;
+                _data->machine.AddState(StateRef(new GameState(_data)), true);
             }
         }
     }
