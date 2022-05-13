cac22967c95edb79968cf5c1e88a7603e7c4b56b
Rahul Banerjee <rbanerjee28@gmail.com>
2022-05-13 13:39:55 +0200

    added game state class

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # flappy-bird
-A simple implementation of the beloved/hated Flappy Bird game. The aim of this repository is to maintain and sharpen my C++ coding skills.
+A simple implementation of the beloved/hated Flappy Bird game, using the SFML library to handle I/O. The aim of this repository is to maintain and sharpen my C++ coding skills.
--- main.cpp
+++ main.cpp
@@ -6,33 +6,5 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT),
-    "Flappy Bird");
-
-    while(window.isOpen())
-    {
-        // handle input event
-        sf::Event event;
-
-        while(window.pollEvent(event))
-        {
-            switch ((event.type))
-            {
-            case sf::Event::Closed:
-                window.close();
-                break;
-            
-            default:
-                break;
-            }
-        }
-
-        // update game logic
-        window.clear();
-
-        //draw objects
-        window.display();
-    }
-
     return EXIT_SUCCESS;
 }
